49ff099340e69289a3e8d2a043eeaf58839a04b2
Sauharda Chatterjee <write2.sauharda.chatterjee@gmail.com>
2026-05-13 16:26:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (12.48%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -17,6 +17,7 @@ class Solution {
         ListNode head1;
         ListNode head2;
         ListNode head3;
+        //edge case 1 that is a single element is to be reversed or linked list has only one element
         if(left==right)
         {
             return head;
@@ -27,6 +28,8 @@ class Solution {
             pos++;
         }
         head2 = ptr2;
+        //what if it is to be reversed from the first element itself
+        //in that case head1 = null and head 1 = head2
         if(left==1)
         {
             head1=null;
@@ -40,6 +43,8 @@ class Solution {
             }
             ptr1.next = null;
         }
+        //doesnt matter if head1 is null
+        //ptr2_2 travels through ptr2 list from the start
         ListNode ptr2_2 = head2;
         while(pos<right)
         {
@@ -48,7 +53,7 @@ class Solution {
         }
         head3 = ptr2_2.next;
         ptr2_2.next = null;
-
+        //Reverse the ptr2 list
         ListNode Prev = null;
 		ListNode Current = head2;
 		ListNode Next = Current.next;
@@ -64,10 +69,14 @@ class Solution {
 				Next=null;
 		}
         head2 = Prev;
+        //reverse code ends here
+        //if head1 is null that is if ptr1 list is empty 
         if(head1==null)
         {
             head1 = head2;
         }
+        //basically navigate ptr2 using head2 if ptr1 is empty 
+        //if ptr1 is not empty get to the end of ptr1 using head 1 
         else
         {
             ListNode ptr1_1 = head1;
@@ -77,12 +86,16 @@ class Solution {
             }
             ptr1_1.next = head2;
         }
+        //at the end of ptr1 join ptr2 
+        //ptr1_2 travels the ptr1 list
         ListNode ptr1_2 = head1;
         while(ptr1_2.next!=null)
         {
             ptr1_2 = ptr1_2.next;
         }
+        //at the end of (ptr1+ptr2) list add the ptr3 list
         ptr1_2.next = head3;
+        // the head of ptr1 can now access all (ptr1+ptr2+ptr3) lists
         return head1;
 
     }
